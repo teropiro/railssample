88e0e68193978dc6afe2b9e30df51a98ac258d4d
teropiro <jamatag@gmail.com>
2013-08-01 22:38:14 +0900

    Finish User SignUp

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Railssample::Application.routes.draw do
-  get "users/new"
-
+  resources :users
+  
   root              to: 'static_pages#home'
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
